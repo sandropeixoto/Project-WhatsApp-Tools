845c872bc52a48eb4b4a4b134c90edd7d5085c7b
Sandro Peixoto <contato@sandropeixoto.com.br>
2026-02-24 08:36:58 -0300

    fix db

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // db.php - Conexão centralizada MySQL
 
-$DB_HOST = 'srv24.prodns.com.br';
+$DB_HOST = '192.185.214.31';
 $DB_NAME = 'sspeixot_whatsapp';
 $DB_USER = 'sspeixot_whatsapp';
 $DB_PASS = 'Senha@2026';
@@ -11,13 +11,14 @@ try {
         "mysql:host={$DB_HOST};dbname={$DB_NAME};charset=utf8mb4",
         $DB_USER,
         $DB_PASS,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ]
-    );
-} catch (PDOException $e) {
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]
+        );
+}
+catch (PDOException $e) {
     http_response_code(500);
     die(json_encode(['error' => 'Falha na conexão com o banco de dados: ' . $e->getMessage()]));
 }
