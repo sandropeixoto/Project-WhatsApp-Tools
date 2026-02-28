543730e43a298dcab23601c031099d0e0578dc49
Sandro Peixoto <contato@sandropeixoto.com.br>
2026-02-28 10:54:05 -0300

    chore: sanitize project and remove legacy/temporary files

--- test_index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$_SERVER['REQUEST_METHOD'] = 'GET';
-$_SERVER['PHP_SELF'] = '/whatsapp/agendar/index.php';
-
-// Mock the user so auth_middleware passes
-session_start();
-$_SESSION['agendar_user'] = ['id' => 1, 'account_id' => 1, 'role' => 'owner'];
-
-require 'agendar/index.php';
